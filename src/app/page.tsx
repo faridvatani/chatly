31c27d22dfb75ef9a84706d42110255509a43951
Farid Vatani <faridvatani@gmail.com>
2024-11-23 18:19:49 +0330

    chore: Setup Shadcn UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <h1>Chatly</h1>;
+  return (
+    <div>
+      <h1>Chatly</h1>
+      <Button>Click me</Button>
+    </div>
+  );
 }
